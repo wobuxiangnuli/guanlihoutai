a29c3f48a9f206d6548bb6d60d7542da4e196182
xiaohu <312977493@qq.com>
2023-07-12 15:40:05 +0800

    初始化项目

---
+++ Web/src/stores/userInfo.ts
@@ -0,0 +1,82 @@
+import { defineStore } from 'pinia';
+import { Local, Session } from '/@/utils/storage';
+import Watermark from '/@/utils/watermark';
+import { useThemeConfig } from '/@/stores/themeConfig';
+
+import { getAPI } from '/@/utils/axios-utils';
+import { SysAuthApi, SysConstApi } from '/@/api-services/api';
+const baseUrl = import.meta.env.VITE_API_URL;
+
+/**
+ * 用户信息
+ * @methods setUserInfos 设置用户信息
+ */
+export const useUserInfo = defineStore('userInfo', {
+	state: (): UserInfosState => ({
+		userInfos: {} as any,
+		constList: [] as any,
+	}),
+	getters: {
+		// 获取系统常量列表
+		async getSysConstList(): Promise<any[]> {
+			var res = await getAPI(SysConstApi).apiSysConstListGet();
+			this.constList = res.data.result ?? [];
+			return this.constList;
+		},
+	},
+	actions: {
+		async setUserInfos() {
+			// 存储用户信息到浏览器缓存
+			if (Session.get('userInfo')) {
+				this.userInfos = Session.get('userInfo');
+			} else {
+				const userInfos = <UserInfos>await this.getApiUserInfo();
+				this.userInfos = userInfos;
+			}
+		},
+		// 获取当前用户信息
+		getApiUserInfo() {
+			return new Promise((resolve) => {
+				getAPI(SysAuthApi)
+					.apiSysAuthUserInfoGet()
+					.then(async (res: any) => {
+						if (res.data.result == null) return;
+						var d = res.data.result;
+						const userInfos = {
+							account: d.account,
+							realName: d.realName,
+							avatar: d.avatar ? d.avatar : '/favicon.ico',
+							address: d.address,
+							signature: d.signature,
+							orgId: d.orgId,
+							orgName: d.orgName,
+							posName: d.posName,
+							roles: [],
+							authBtnList: d.buttons,
+							time: new Date().getTime(),
+						};
+						// vue-next-admin 提交Id：225bce7 提交消息：admin-23.03.26:发布v2.4.32版本
+						// 增加了下面代码，引起当前会话的用户信息不会刷新，如：重新提交的头像不更新，需要新开一个页面才能正确显示
+						// Session.set('userInfo', userInfos);
+
+						// 读取用户配置
+						const configRes: any = await getAPI(SysAuthApi).apiSysAuthUserConfigGet();
+						if (configRes.data.result == null) return;
+
+						const configData = configRes.data.result;
+						const storesThemeConfig = useThemeConfig();
+
+						// 是否设置水印
+						storesThemeConfig.themeConfig.isWatermark = configData.watermarkEnabled;
+						if (storesThemeConfig.themeConfig.isWatermark) Watermark.set(storesThemeConfig.themeConfig.watermarkText);
+						else Watermark.del();
+
+						Local.remove('themeConfig');
+						Local.set('themeConfig', storesThemeConfig.themeConfig);
+
+						resolve(userInfos);
+					});
+			});
+		},
+	},
+});
